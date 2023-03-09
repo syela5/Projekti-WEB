af07802bcae43b0a196e698006e454c0da297468
Albina.H <albinahalabaku8@gmail.com>
2023-03-09 13:41:01 +0100

    Lidhja me dashboard dhe home

--- Admin/admin_header.php
+++ Admin/admin_header.php
@@ -15,14 +15,14 @@ if(isset($message)){
 
    <div class="flex">
 
-      <a href="admin_page.php" class="logo">Admin<span>Panel</span></a>
+      <a href="admin_page.php" class="logo">Dashboard</span></a>
 
       <nav class="navbar">
-         <a href="admin_page.php">home</a>
-         <a href="admin_products.php">products</a>
-         <a href="admin_orders.php">orders</a>
-         <a href="admin_users.php">users</a>
-         <a href="admin_contacts.php">messages</a>
+         <a href="admin_page.php">Home</a>
+         <a href="admin_products.php">Destination</a>
+         <a href="admin_orders.php">Booking</a>
+         <a href="admin_users.php">Users</a>
+         <a href="admin_contacts.php">Messages</a>
       </nav>
 
       <div class="icons">
--- Admin/register.php
+++ Admin/register.php
@@ -1,48 +1,31 @@
 <?php
-
-include '../config/dbconnect.php';
+// lidhja me databaze
+include '../Config/dbconnect.php';
 
 if(isset($_POST['submit'])){
 
-    $name = mysqli_real_escape_string($conn, $_POST['name']);
-    $email = mysqli_real_escape_string($conn, $_POST['email']);
-    $pass = mysqli_real_escape_string($conn, md5($_POST['password']));
-    $cpass = mysqli_real_escape_string($conn, md5($_POST['cpassword']));
-    $user_type = $_POST['user_type'];
- 
-    $message = array();
+   $name = mysqli_real_escape_string($conn, $_POST['name']);
+   $email = mysqli_real_escape_string($conn, $_POST['email']);
+   $pass = mysqli_real_escape_string($conn, md5($_POST['password']));
+   $cpass = mysqli_real_escape_string($conn, md5($_POST['cpassword']));
+   $user_type = $_POST['user_type'];
 
-    if(empty($name)){
-        $errors[] = 'Please enter your name';
-     }
-     if(empty($email)){
-        $errors[] = 'Please enter your email';
-     }
-     if(empty($pass)){
-        $errors[] = 'Please enter your password';
-     }
-     if(empty($cpass)){
-        $errors[] = 'Please enter your confirm password';
-     }
-    // check if password has at least 6 characters
-    if(strlen($_POST['password']) < 6){
-       $message[] = 'Password must have at least 6 characters';
-    }
-    $select_users = mysqli_query($conn, "SELECT * FROM `users` WHERE email = '$email' AND password = '$pass'") or die('query failed');
- 
-    if(mysqli_num_rows($select_users) > 0){
-       $message[] = 'User already exists!';
-    }else{
-       if($pass != $cpass){
-          $message[] = 'Confirm password not matched!';
-       }else{
-          mysqli_query($conn, "INSERT INTO `users`(name, email, password, user_type) VALUES('$name', '$email', '$cpass', '$user_type')") or die('query failed');
-          $message[] = 'Registered successfully!';
-          header('location:login.php');
-       }
-    }
- 
- }
+   $select_users = mysqli_query($conn, "SELECT * FROM `users` WHERE email = '$email' AND password = '$pass'") or die('query failed');
+
+   if(mysqli_num_rows($select_users) > 0){
+      $message[] = 'user already exist!';
+   }else{
+      if($pass != $cpass){
+         $message[] = 'confirm password not matched!';
+      }else{
+         mysqli_query($conn, "INSERT INTO `users`(name, email, password, user_type) VALUES('$name', '$email', '$cpass', '$user_type')") or die('query failed');
+         $message[] = 'registered successfully!';
+         header('location:login.php');
+      }
+   }
+
+}
+
 ?>
 
 <!DOCTYPE html>
@@ -56,7 +39,7 @@ if(isset($_POST['submit'])){
    <!-- font awesome cdn link  -->
    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
 
-   <!-- custom css file link  -->
+   <!-- style per register form  -->
    <link rel="stylesheet" href="css/style.css">
 
 </head>
@@ -90,7 +73,7 @@ if(isset($message)){
          <option value="admin">admin</option>
       </select>
       <input type="submit" name="submit" value="register now" class="btn">
-      <p>I have an account <br> <a href="login.php">Login now</a></p>
+      <p>I have an accound <br> <a href="login.php">Login now</a></p>
    </form>
 
 </div>
